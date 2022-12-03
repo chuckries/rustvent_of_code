e195c6af85d6d49278d72517e5db360830317506
Chuck Ries <chuckr@microsoft.com>
2022-12-03 13:55:45 -0800

    tinker

--- crates/aoc_2022/src/day03.rs
+++ crates/aoc_2022/src/day03.rs
@@ -2,15 +2,14 @@ use std::collections::HashSet;
 
 use aoc_common::file_lines;
 
-
 fn input() -> impl Iterator<Item = String> {
     file_lines("inputs/day03.txt")
 }
 
 fn letter_score(c: u8) -> u32 {
     let score = match c {
-        c @ b'a'..=b'z' => c - b'a' + 1,
-        c @ b'A'..=b'Z' => c - b'A' + 27,
+        b'a'..=b'z' => c - b'a' + 1,
+        b'A'..=b'Z' => c - b'A' + 27,
         _ => panic!()
     };
     score as u32
@@ -20,7 +19,6 @@ fn letter_score(c: u8) -> u32 {
 fn part1() {
     let answer: u32 = input().map(|line| {
         let (front, back) = line.split_at(line.len() / 2);
-
         let front: HashSet<u8> = front.bytes().collect();
         let back: HashSet<u8> = back.bytes().collect();
 
@@ -40,9 +38,7 @@ fn part2() {
         let c2: HashSet<u8> = chunk[2].bytes().collect();
 
         let similar: HashSet<u8> = c0.intersection(&c1).copied().collect();
-        let similar = similar.intersection(&c2);
-
-        similar.copied().map(letter_score).sum::<u32>()
+        similar.intersection(&c2).copied().map(letter_score).sum::<u32>()
     }).sum();
 
     assert_eq!(answer, 2499);

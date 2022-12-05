7bc483a7b759f89fb53b5480e27e5a80f3d07ff8
Chuck Ries <chuckr@microsoft.com>
2022-12-04 21:47:50 -0800

    cleanup

--- crates/aoc_2022/src/day05.rs
+++ crates/aoc_2022/src/day05.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use aoc_common::{file_lines, IteratorExt};
 
 
@@ -5,7 +7,7 @@ fn input() -> (Vec<Vec<char>>, impl Iterator<Item = (usize, usize, usize)>)
 {
     let mut lines = file_lines("inputs/day05.txt");
 
-    let mut stacks: Vec<Vec<char>> = Vec::new();
+    let mut stacks: Vec<VecDeque<char>> = Vec::new();
 
     while let Some(line) = lines.next() {
         let chars = line.chars().to_vec();
@@ -16,16 +18,12 @@ fn input() -> (Vec<Vec<char>>, impl Iterator<Item = (usize, usize, usize)>)
             if chars[i] == '[' {
                 let stack_idx = i / 4;
                 while stack_idx >= stacks.len() {
-                    stacks.push(Vec::new());
+                    stacks.push(VecDeque::new());
                 }
-                stacks[stack_idx].push(chars[i + 1]);
+                stacks[stack_idx].push_front(chars[i + 1]);
             }
             i += 4;
         }
-    }
-
-    for s in stacks.iter_mut() {
-        s.reverse();
     }
 
     lines.next();
@@ -36,32 +34,39 @@ fn input() -> (Vec<Vec<char>>, impl Iterator<Item = (usize, usize, usize)>)
         (split[1].parse().unwrap(), split[3].parse().unwrap(), split[5].parse().unwrap())
     });
 
-    (stacks, instructions)
+    (stacks.into_iter().map(|v| v.into_iter().collect()).collect(), instructions)
 
+}
+
+fn run<F>(mut f: F) -> String
+    where F: FnMut(usize, usize, usize, &mut Vec<Vec<char>>)
+{
+    let (mut stacks, instructions) = input();
+
+    for (count, from, to) in instructions {
+        f(count, to, from, &mut stacks);
+    }
+
+    stacks.iter().map(|s| s.last().unwrap()).collect()
 }
 
 #[test]
 fn part1() {
-    let (mut stacks, instructions) = input();
-
-    for (count, from, to) in instructions {
+    let answer = run(|count, to, from, stacks| {
         for _ in 0..count {
             let c = stacks[from - 1].pop().unwrap();
             stacks[to - 1].push(c);
         }
-    }
+    });
 
-    let answer: String = stacks.iter().map(|s| s.last().unwrap()).collect();
-
-    assert_eq!(answer, "");
+    assert_eq!(answer, "VQZNJMWTR");
 }
 
 #[test]
 fn part2() {
-    let (mut stacks, instructions) = input();
     let mut tmp: Vec<char> = Vec::new();
 
-    for (count, from, to) in instructions {
+    let answer = run(|count, to, from, stacks| {
         for _ in 0..count {
             tmp.push(stacks[from - 1].pop().unwrap());
         }
@@ -69,9 +74,7 @@ fn part2() {
         while let Some(c) = tmp.pop() {
             stacks[to - 1].push(c);
         }
-    }
+    });
 
-    let answer: String = stacks.iter().map(|s| s.last().unwrap()).collect();
-
-    assert_eq!(answer, "");
+    assert_eq!(answer, "NLCDCLVMQ");
 }

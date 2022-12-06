ea88f13516ca0be88fceb4801f3cac50416b6ba5
Chuck Ries <chuckr@microsoft.com>
2022-12-05 21:29:08 -0800

    optimize

--- crates/aoc_2022/src/day06.rs
+++ crates/aoc_2022/src/day06.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap};
+
 use aoc_common::{file_string, IteratorExt};
 
 
@@ -5,20 +7,47 @@ fn input() -> String {
     file_string("inputs/day06.txt")
 }
 
+// too good not to keep
+// fn find(n: usize) -> usize {
+//     input()
+//         .chars()
+//         .enumerate()
+//         .to_vec()
+//         .windows(n)
+//         .filter(|w| 
+//             w.iter()
+//                 .map(|p| p.1)
+//                 .to_set()
+//                 .len() == n
+//             )
+//         .next()
+//         .unwrap()[n - 1].0 + 1
+// }
+
 fn find(n: usize) -> usize {
-    input()
-        .chars()
-        .enumerate()
-        .to_vec()
-        .windows(n)
-        .filter(|w| 
-            w.iter()
-                .map(|p| p.1)
-                .to_set()
-                .len() == n
-            )
-        .next()
-        .unwrap()[n - 1].0 + 1
+    let chars = input().chars().to_vec();
+    let mut map: HashMap<char, usize> = HashMap::new();
+
+    for i in 0..n {
+        *map.entry(chars[i]).or_default() += 1;
+    }
+
+    let mut front = 0;
+    let mut back = n;
+
+    loop {
+        if map.values().all(|v| *v == 0 || *v == 1) {
+            break;
+        }
+
+        *map.get_mut(&chars[front]).unwrap() -=1 ;
+        *map.entry(chars[back]).or_default() += 1;
+
+        front += 1;
+        back += 1;
+    }
+
+    back
 }
 
 #[test]

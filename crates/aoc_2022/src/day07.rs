621a435cd3f0a8ebc795bd2323a922128be3b4e2
Chuck Ries <chuckr@microsoft.com>
2022-12-07 00:31:44 -0800

    refactor

--- crates/aoc_2022/src/day07.rs
+++ crates/aoc_2022/src/day07.rs
@@ -1,89 +1,68 @@
+use std::collections::HashMap;
 use aoc_common::{file_lines, IteratorExt};
+use Node::*;
 
-use Kind::*;
-
-struct Node {
-    name: String,
-    kind: Kind,
-}
-
-enum Kind {
-    Dir(Vec<Node>),
+enum Node {
+    Dir(HashMap<String, Node>),
     File(usize),
 }
 
-fn explore(idx: &mut usize, lines: &[String], cd: &mut Vec<Node>) {
-    while *idx < lines.len() {
-        let line = &lines[*idx];
-        *idx += 1;
+impl Node {
+    fn as_dir(&mut self) -> &mut HashMap<String, Node> {
+        match self {
+            Dir(map) => map,
+            _ => panic!()
+        }
+    }
+}
 
-        if line.starts_with('$') {
-            let split = line.split(' ').to_vec();
-
+fn input() -> Node {
+    fn explore(mut idx: usize, lines: &[String], cd: &mut HashMap<String, Node>) -> usize {
+        while idx < lines.len() {
+            let split = lines[idx].split(' ').to_vec();
+            idx += 1;
+    
+            if split[0] != "$" { panic!(); }
+    
             match split[1] {
-                "cd" => {
-                    let name = split[2];
-                    if name == ".." {
-                        return;
-                    } else {
-                        let dir = cd.iter_mut().find(|d| d.name == name).unwrap();
-                        if let Dir(dir) = &mut dir.kind {
-                            explore(idx, lines, dir);
-                        } else {
-                            panic!()
-                        }
-                    }
+                "cd" => match split[2] {
+                    ".." => break,
+                    next @ _ => idx = explore(idx, lines, cd.get_mut(next).unwrap().as_dir()),
                 }
                 "ls" => {
-                    while *idx < lines.len() {
-                        let split = lines[*idx].split(' ').to_vec();
+                    while idx < lines.len() {
+                        let split = lines[idx].split(' ').to_vec();
                         match split[0] {
                             "$" => break,
-                            "dir" => {
-                                cd.push(Node { name: split[1].to_string(), kind: Dir(Vec::new()) });
-                            }
-                            _ => {
-                                let size = split[0].parse().unwrap();
-                                let name = split[1].to_string();
-                                cd.push(Node { name: name, kind: File(size) });
-                            }
-                        }
-                        *idx += 1;
+                            "dir" => cd.insert(split[1].to_string(), Dir(HashMap::new())),
+                            size @ _ => cd.insert(split[1].to_string(), File(size.parse().unwrap())),
+                        };
+    
+                        idx += 1;
                     }
                 }
                 _ => panic!()
             }
-        } else {
-            panic!();
         }
+    
+        idx
     }
+
+    let mut lines = file_lines("inputs/day07.txt").to_vec();
+    let mut root: HashMap<String, Node> = HashMap::new();
+    root.insert("/".to_string(), Dir(HashMap::new()));
+    _ = explore(0, &mut lines, &mut root);
+    root.into_iter().next().unwrap().1
 }
 
-fn sum(node: &Node, running_total: &mut usize) -> usize {
-    match &node.kind {
+fn sum_visit<F>(node: &Node, f: &mut F) -> usize
+    where F: FnMut(usize)
+{
+    match node {
         File(size) => *size,
         Dir(nodes) => {
-            let sum = nodes.iter().map(|n| sum(n, running_total)).sum();
-
-            if sum <= 100000 {
-                *running_total += sum;
-            }
-
-            sum
-        }
-    }
-}
-
-fn find_to_delete(node: &Node, min_required: usize, min: &mut usize) -> usize {
-    match &node.kind {
-        File(size) => *size,
-        Dir(nodes) => {
-            let sum = nodes.iter().map(|n| find_to_delete(n, min_required, min)).sum();
-
-            if sum >= min_required && sum < *min {
-                *min = sum;
-            }
-
+            let sum = nodes.iter().map(|(_, n)| sum_visit(n, f)).sum();
+            f(sum);
             sum
         }
     }
@@ -91,15 +70,12 @@ fn find_to_delete(node: &Node, min_required: usize, min: &mut usize) -> usize {
 
 #[test]
 fn part1() {
-    let lines = file_lines("inputs/day07.txt").to_vec();
-    let mut root: Vec<Node> = Vec::new();
-    root.push(Node { name: "/".to_string(), kind: Dir(Vec::new()) });
-
-    let mut idx: usize = 0;
-    explore(&mut idx, &lines, &mut root);
-
     let mut answer = 0;
-    sum(&root[0], &mut answer);
+    sum_visit(&input(), &mut |s| {
+        if s <= 100000 {
+            answer += s;
+        }
+    });
 
     assert_eq!(answer, 1723892);
 }
@@ -109,20 +85,13 @@ const UNUSED_NEEDED: usize = 30000000;
 
 #[test]
 fn part2() {
-    let lines = file_lines("inputs/day07.txt").to_vec();
-    let mut root: Vec<Node> = Vec::new();
-    root.push(Node { name: "/".to_string(), kind: Dir(Vec::new()) });
-
-    let mut idx: usize = 0;
-    explore(&mut idx, &lines, &mut root);
-
-    let mut whatever = 0;
-    let total = sum(&root[0], &mut whatever);
-
-    let min_to_delete = UNUSED_NEEDED - (AVAILABLE - total);
-
     let mut answer = usize::MAX;
-    find_to_delete(&root[0], min_to_delete, &mut answer);
+    let min_to_delete = UNUSED_NEEDED - (AVAILABLE - sum_visit(&input(), &mut |_| { }));
+    sum_visit(&input(), &mut |s| {
+        if s > min_to_delete && s < answer {
+            answer = s;
+        }
+    });
 
     assert_eq!(answer, 8474158);
 }

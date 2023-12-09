9e25bf6d5dac051643ded58def9331edf3b556cc
Chuck Ries <chuckr@microsoft.com>
2023-12-09 00:46:42 -0800

    day 9 tweak

--- crates/aoc_2023/src/day09.rs
+++ crates/aoc_2023/src/day09.rs
@@ -6,13 +6,11 @@ fn input() -> Vec<Vec<i64>> {
     }).to_vec()
 }
 
-fn extrapolate<S, O>(seed: Vec<i64>, selector: &S, operation: &O) -> i64 
-    where S: Fn(&[i64]) -> i64, O: Fn(i64, i64) -> i64
-{
+fn extrapolate(seed: Vec<i64>) -> i64 {
     let mut stack: Vec<i64> = Vec::new();
     let mut current = seed;
     loop {
-        stack.push(selector(&current));
+        stack.push(current[current.len() - 1]);
         let next = current.windows(2).map(|w| w[1] - w[0]).to_vec();
         if next.iter().all(|n| *n == 0) {
             break;
@@ -20,30 +18,24 @@ fn extrapolate<S, O>(seed: Vec<i64>, selector: &S, operation: &O) -> i64
         current = next;
     }
 
-    let mut accum = 0;
-    while let Some(n) = stack.pop() {
-        accum = operation(accum, n);
-    }
-
-    accum
-}
-
-fn run<S, O>(selector: S, operation: O) -> i64 
-    where S: Fn(&[i64]) -> i64, O: Fn(i64, i64) -> i64
-{
-    input().into_iter().map(|seed| {
-        extrapolate(seed, &selector, &operation)
-    }).sum()
+    stack.iter().sum()
 }
 
 #[test]
 fn part1() {
-    let answer = run(|seq| *seq.last().unwrap(), |acc, x| acc + x);
+    let answer: i64 = input().into_iter().map(|seed| {
+        extrapolate(seed)
+    }).sum();
+
     assert_eq!(1789635132, answer);
 }
 
 #[test]
 fn part2() {
-    let answer = run(|seq| seq[0], |acc, x| x - acc);
+    let answer: i64 = input().into_iter().map(|mut seed| {
+        seed.reverse();
+        extrapolate(seed)
+    }).sum();
+
     assert_eq!(913, answer);
 }

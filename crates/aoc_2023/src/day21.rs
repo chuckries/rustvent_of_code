abb4ada1a3d76c2aa2d11cee6c4cdd209ddeab89
Chuck Ries <chuckr@microsoft.com>
2023-12-21 10:03:02 -0800

    day 21 part 2

--- crates/aoc_2023/src/day21.rs
+++ crates/aoc_2023/src/day21.rs
@@ -64,26 +64,44 @@ fn print_space<W: std::io::Write, F>(space: &Vec<Vec<Vec<Vec<char>>>>, steps: &H
     }
 }
 
+fn count_positions(map: &Vec<Vec<char>>, start: Vec2us, iterations: &[usize], print: bool) -> Vec<usize> {
+    let bounds = Vec2us::new(map[0].len(), map.len());
+    let mut positions: HashSet<Vec2us> = HashSet::new();
+    let mut next = positions.clone();
+    positions.insert(start);
+
+    let mut counts: Vec<usize> = Vec::new();
+    let mut loop_count = 0;
+    for iteration in iterations {
+        while loop_count < *iteration {
+            for p in positions.iter() {
+                for adj in p.adjacent_bounded(&bounds) {
+                    if map[adj.y][adj.x] == '.' {
+                        next.insert(adj);
+                    }
+                }
+            }
+
+            positions.clear();
+            (positions, next) = (next, positions);
+
+            loop_count += 1;
+        }
+
+        counts.push(positions.len());
+
+        if print {
+            print_map(map, &positions, &mut std::io::stdout());
+        }
+    }
+
+    counts
+}
+
 #[test]
 fn part1() {
     let (map, start) = input();
-    let bounds = Vec2us::new(map[0].len(), map.len());
-    let mut positions: HashSet<Vec2us> = HashSet::new();
-    positions.insert(start);
-
-    for _ in 0..64 {
-        let mut next: HashSet<Vec2us> = HashSet::new();
-        for p in positions {
-            for adj in p.adjacent_bounded(&bounds) {
-                if map[adj.y][adj.x] == '.' {
-                    next.insert(adj);
-                }
-            }
-        }
-        positions = next;
-    }
-
-    let answer = positions.len();
+    let answer = count_positions(&map, start, &[64], false)[0];
     assert_eq!(3642, answer);
 }
 
@@ -120,62 +138,81 @@ fn part2() {
        
      */
 
-    const STEPS: usize = 26501365;
-
     let (map, start) = input();
-    let bounds = Vec2us::new(map[0].len(), map.len());
+    let size = map.len();
+    let half_size = map.len() / 2;
 
-    let space = vec![vec![map; 5]; 5];
+    let nw_edges = count_positions(&map, Vec2us::new(size - 1, size - 1), &[half_size - 1, size + half_size - 1], false);
+    let nw_thin = nw_edges[0];
+    let nw_thick = nw_edges[1];
 
-    let p_to_space = |mut p: Vec2i32| -> Option<(Vec2us, Vec2us)> {
-        let mut uv = Vec2i32::new(2, 2);
-        while p.x < 0 {
-            p.x += bounds.x as i32;
-            uv.x -= 1;
-        }
-        while p.x >= bounds.x as i32 {
-            p.x -= bounds.x as i32;
-            uv.x += 1;
-        }
-        while p.y < 0 {
-            p.y += bounds.y as i32;
-            uv.y -= 1;
-        }
-        while p.y >= bounds.y as i32 {
-            p.y -= bounds.y as i32;
-            uv.y += 1;
-        }
+    let sw_edges = count_positions(&map, Vec2us::new(size - 1, 0), &[half_size - 1, size + half_size - 1], false);
+    let sw_thin = sw_edges[0];
+    let sw_thick = sw_edges[1];
 
-        if uv.x < 0 || uv.x >= 5 || uv.y < 0 || uv.y >= 5 {
-            return None;
-        }
+    let ne_edges = count_positions(&map, Vec2us::new(0, size - 1), &[half_size - 1, size + half_size - 1], false);
+    let ne_thin = ne_edges[0];
+    let ne_thick = ne_edges[1];
 
-        Some((uv.cast(), p.cast()))
-    };
+    let se_edges = count_positions(&map, Vec2us::new(0, 0), &[half_size - 1, size + half_size - 1], false);
+    let se_thin = se_edges[0];
+    let se_thick = se_edges[1];
 
-    let mut file = File::create("steps.txt").unwrap();
+    let n_point = count_positions(&map, Vec2us::new(half_size, size - 1), &[size - 1], false)[0];
+    let s_point = count_positions(&map, Vec2us::new(half_size, 0), &[size - 1], false)[0];
+    let w_point = count_positions(&map, Vec2us::new(size - 1, half_size), &[size - 1], false)[0];
+    let e_point = count_positions(&map, Vec2us::new(0, half_size), &[size - 1], false)[0];
 
-    let mut positions: HashSet<Vec2i32> = HashSet::new();
-    let start = Vec2us::new(0, 0);
-    positions.insert(start.cast());
+    let centers = count_positions(&map, start, &[half_size + size - 1, half_size + size], false);
+    let center_0 = centers[0];
+    let cetner_1 = centers[1];
 
-    for _ in 0..(64 + 131) {
-        let mut next: HashSet<Vec2i32> = HashSet::new();
-        for p in positions {
-            for adj in p.adjacent() {
-                if let Some((uv, p)) = p_to_space(adj) {
-                    if space[uv.y][uv.x][p.y][p.x] == '.' {
-                        next.insert(adj);
-                    }
-                }
-            }
-        }
-        println!("{}", next.len());
-        positions = next;
-    }
+    const ITERATIONS: usize = 26501365;
 
-    print_space(&space, &positions, &mut file, p_to_space);
+    // calculate height of map in "tiles", this number is from the center line to one boundary (inclusive)
+    let height_in_one_dir_inclusive = (ITERATIONS - half_size) / size + 1;
 
-    let answer = positions.len();
-    assert_eq!(3642, answer);
+    // this is the numbef of tiles between the center line and a boundary (exclusive)
+    let interior_distance = height_in_one_dir_inclusive - 2;
+
+    // calculate the total number of edge tiles on one edge of the diamond
+    // every row between center and boundary (exlusive) will have 1 thin and 1 thick
+    // the boundary row will have an additional thin
+    let thicks = interior_distance;
+    let thins = thicks + 1;
+
+    // count the interior tiles, there is probably a forumla this but idk
+    // this counts primary and alternate interior tiles between the center line and a boundary (exclusive)
+    // first line inside the boundary will have one alt, next will have two alts and 1 primary, next will have 3 alts and 2 primary, etc.
+    // 0: (0, 1)
+    // 1: (1, 2)
+    // 2: (2, 3)
+    // n: (n, n + 1)
+
+    let primaries = (interior_distance * (interior_distance - 1) ) / 2;
+    let alternates = ((interior_distance + 1) * interior_distance) / 2;
+
+    // double both of them
+    let primaries = primaries * 2;
+    let alternates = alternates * 2;
+
+    // add the center line
+    let interior_width_of_center_line = 2 * height_in_one_dir_inclusive - 3;
+    let primaries = primaries + interior_width_of_center_line / 2;
+    let alternates = alternates + interior_width_of_center_line / 2 + 1;
+
+    let mut total: usize = 0;
+
+    // add all the edges
+    total += thins * (nw_thin + ne_thin + sw_thin + se_thin);
+    total += thicks * (nw_thick + ne_thick + sw_thick + se_thick);
+
+    // add the points
+    total += n_point + s_point + w_point + e_point;
+
+    // add the interiors
+    total += primaries * center_0;
+    total += alternates * cetner_1;
+
+    assert_eq!(93270, total);
 }

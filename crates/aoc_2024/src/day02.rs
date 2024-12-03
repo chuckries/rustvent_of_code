98dfd77f30765b00c99e5310d3939da872a72735
Chuck Ries <chuckr@microsoft.com>
2024-12-03 10:01:03 -0800

    day 2 rework

--- crates/aoc_2024/src/day02.rs
+++ crates/aoc_2024/src/day02.rs
@@ -6,62 +6,47 @@ fn input() -> Vec<Vec<i32>> {
     }).to_vec()
 }
 
-#[test]
-fn part1() {
-    let input = input();
+fn run<F>(f: F) -> usize 
+    where F: Fn(&[i32]) -> bool
+{
+    input().into_iter().filter(|l| f(&l)).count()
+}
 
-    let answer = input.into_iter().filter(|list| {
-        let delta = list[0] - list[1];
-        if matches!(delta.abs(), i if i < 1 || i > 3) {
+fn test(list: &[i32]) -> bool {
+    let ord = list[0].cmp(&list[1]);
+
+    for w in list.windows(2) {
+        if w[0].cmp(&w[1]) != ord {
             return false;
         }
-        let sign = delta.signum();
 
-        for w in list.windows(2).skip(1) {
-            let delta = w[0] - w[1];
-            if delta.signum() != sign {
-                return false;
-            }
-            if matches!(delta.abs(), i if i < 1 || i > 3) {
-                return false;
-            }
+        if matches!((w[0] - w[1]).abs(), i if i < 1 || i > 3) {
+            return false;
         }
+    }
 
-        true
-    }).count();
+    true
+}
 
+fn test_2(list: &[i32]) -> bool {
+    for i in 0..list.len() {
+        let mut list = list.to_vec();
+        list.remove(i);
+        if test(&list) {
+            return true;
+        }
+    }
+    false
+}
+
+#[test]
+fn part1() {
+    let answer = run(test);
     assert_eq!(answer, 326);
 }
 
 #[test]
 fn part2() {
-    let input = input();
-
-    let answer = input.into_iter().filter(|list| {
-        'outer: for i in 0..list.len() {
-            let mut list = list.clone();
-            list.remove(i);
-            let list = list;
-            let delta = list[0] - list[1];
-            if matches!(delta.abs(), i if i < 1 || i > 3) {
-                continue 'outer;
-            }
-            let sign = delta.signum();
-    
-            for w in list.windows(2).skip(1) {
-                let delta = w[0] - w[1];
-                if delta.signum() != sign {
-                    continue 'outer;
-                }
-                if matches!(delta.abs(), i if i < 1 || i > 3) {
-                    continue 'outer;
-                }
-            }
-    
-            return true;
-        }
-        false
-    }).count();
-
+    let answer= run(test_2);
     assert_eq!(answer, 381);
 }
